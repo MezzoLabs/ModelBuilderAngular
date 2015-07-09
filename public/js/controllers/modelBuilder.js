4055c82b9d925cf16d37967c807e62c9e29c41aa
Marc <marc.vornetran@gmail.com>
2015-07-09 11:10:26 +0200

    Layout for model builder

--- public/js/controllers/modelBuilder.js
+++ public/js/controllers/modelBuilder.js
@@ -1,3 +1,15 @@
 module.exports = /*@ngInject*/ $scope => {
+    $scope.tab = 'add';
+    $scope.fields = [];
 
+    $scope.showTab = ($event, tab) => {
+        $scope.tab = tab;
+
+        $event.preventDefault();
+        $(this).tab('show');
+    };
+
+    $scope.addField = field => {
+        $scope.fields.push(field);
+    };
 };
