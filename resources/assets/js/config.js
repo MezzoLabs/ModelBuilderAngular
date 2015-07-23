6af48b9d11ae7daf12a011dc9ad7c9b4eb908744
marc1404 <marc.vornetran@gmail.com>
2015-07-23 18:12:47 +0200

    Use html5 mode

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -1,6 +1,8 @@
 var routes = require('./routes');
 
-module.exports = /*@ngInject*/ function config($routeProvider){
+module.exports = /*@ngInject*/ function config($locationProvider, $routeProvider){
+    $locationProvider.html5Mode(true);
+
     routes.forEach(route => {
        $routeProvider.when(route.when, route);
     });
