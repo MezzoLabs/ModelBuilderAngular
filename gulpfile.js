34dae1a7c95ca33e7690cd5df1a630c4bb717393
marc1404 <marc.vornetran@gmail.com>
2015-07-23 18:03:22 +0200

    Gulp: copy html to public folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.browserify('app.js');
+    mix.task('html', 'resources/assets/html/**/*');
+});
+
+gulp.task('html', function(){
+    return gulp.src('resources/assets/html/**/*').pipe(gulp.dest('public'));
 });
