6d10f85446fce4454ba55a07438e46c9aa36b52c
marc1404 <marc.vornetran@gmail.com>
2015-07-23 18:54:00 +0200

    Use gulp template cache

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,22 @@ var ngRegister = require('gulp-ng-register');
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
+    mix.task('html', 'resources/assets/html/**/*');
+    mix.task('template-cache', 'resources/assets/js/**/*.html');
     mix.task('register', 'resources/assets/js/**/*');
     mix.sass('app.scss');
     mix.browserify('app.js');
-    mix.task('html', 'resources/assets/html/**/*');
 });
 
 gulp.task('html', function(){
     return gulp.src('resources/assets/html/**/*')
         .pipe(gulp.dest('public'));
+});
+
+gulp.task('template-cache', function(){
+    return gulp.src('resources/assets/js/**/*.html')
+        .pipe(templateCache())
+        .pipe(gulp.dest('resources/assets/js'));
 });
 
 gulp.task('register', function(){
