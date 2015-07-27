c272eaec3fcc072467dcd0fc10dd20d034a628da
Marc <marc@mezzolabs.io>
2015-07-27 15:50:56 +0200

    Use and implement angular-sortable-view again

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mezzo-model-builder', [ 'ngRoute', 'templates' ]);
+var app = angular.module('mezzo-model-builder', [ 'ngRoute', 'templates', 'angular-sortable-view' ]);
 
 app.config(require('./config'));
 require('./register')(app);
